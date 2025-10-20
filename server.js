eda5bd86b8e5c23c047b708397c66c3a6e9f52f0
coolkidkanyee <coolkidkanyee@gmail.com>
2025-10-20 11:32:49 -0700

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const app = express();
-app.use(express.static(".")); // serve all files from root
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running on port", process.env.PORT || 3000);
-});
